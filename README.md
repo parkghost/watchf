6283667c377cfd3508b870b62c60e7533789cf3b
Brandon Chen <parkghost@gmail.com>
2013-04-30 11:15:09 +0800

    update version to 0.1.4

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Watchf(v0.1.3)
+Watchf(v0.1.4)
 -------
 
 *Watchf is a tool to execute commands when file changes*
--- watchf.go
+++ watchf.go
@@ -18,7 +18,7 @@ import (
 )
 
 const (
-	Version         = "0.1.3"
+	Version         = "0.1.4"
 	PidFile         = "watchf.pid"
 	Program         = "watchf"
 	ContinueOnError = false
